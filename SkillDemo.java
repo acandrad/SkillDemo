65b1f94f9ca679aa8bdbd54f4a6c399816ed0752
Alexander C Andrade <cs15lsp22anw@ieng6-201.ucsd.edu>
2022-05-07 13:15:45 -0700

    Updated SkillDemo.java from remote acount

--- SkillDemo.java
+++ SkillDemo.java
@@ -2,6 +2,6 @@ public class SkillDemo {
 
     public static int multiplyBy100(int n) {
         return n*100;
-	System.out.println("This line was added from the remote server");
+	System.out.println("This line was added and pushed from the remote server");
     }
 }
